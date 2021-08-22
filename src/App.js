6815f0e7a110449be0599db93ee67e10581baeef
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2021-08-22 22:02:18 +0300

    setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import IntaSend from "intasend-inlinejs-sdk";
 
 function App() {
+  const PUBLIC_KEY = "ISPubKey_test_91ffc81a-8ac4-419e-8008-7091caa8d73f";
+
+    let instance = new IntaSend({
+      publicAPIKey: PUBLIC_KEY,
+      live: false // Set to true when going live
+    });
+
+    const handleClick = () => {
+      instance
+        .run({ amount: 10, currency: "KES" })
+        .on("COMPLETE", (results) => console.log("COMPLETE", results))
+        .on("FAILED", (results) => console.log("FAILED", results));
+    };
+
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <button onClick={handleClick} className="btn intasend-btn">
+        Pay now KES 10
+      </button>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +35,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
