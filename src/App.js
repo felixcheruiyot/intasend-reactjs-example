a202a81c890b1093598f78eb5f5f6be3f2886279
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2021-08-22 22:37:13 +0300

    UseEffect and useCallback to handle events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import IntaSend from "intasend-inlinejs-sdk";
+import { useCallback, useEffect } from 'react';
 
 function App() {
   const PUBLIC_KEY = "ISPubKey_test_91ffc81a-8ac4-419e-8008-7091caa8d73f";
@@ -13,27 +14,38 @@ function App() {
     const handleClick = () => {
       instance
         .run({ amount: 10, currency: "KES", api_ref: "<ORDER-NUMBER-OR-USER-ID>" })
-    };
+    }
 
     // Listen for events
-    function bindEvent(element, eventName, eventHandler) {
+    const bindEvent =useCallback((element, eventName, eventHandler) => {
         if (element.addEventListener) {
             element.addEventListener(eventName, eventHandler, false);
         } else if (element.attachEvent) {
             element.attachEvent('on' + eventName, eventHandler);
         }
-    }
+    }, []);
 
-    bindEvent(window, 'message', function (e) {
-      if (e.data.message) {
-          if (e.data.message.identitier === 'intasend-status-update-cdrtl') {
-              if (e.data.message.state === "COMPLETE") {
+    const handleMessageEvent = useCallback((e) =>{
+      const res = e.data.message
+      if (res) {
+          if (res.identitier === 'intasend-status-update-cdrtl') {
+              if (res.state === "COMPLETE") {
                   // Do something on pay success
-                  alert("Complete:"+JSON.stringify(e.data.message))
+                  alert("Complete:"+JSON.stringify(res))
               }
           }
       }
-  })
+    }, []);
+    useEffect(() => {
+      bindEvent(window, 'message', handleMessageEvent);
+            return () => {
+                if (window.addEventListener) {
+                      window.removeEventListener('message', handleMessageEvent);
+                } else if (window.attachEvent) {
+                      window.detachEvent('onmessage', handleMessageEvent);
+                }
+            }
+      }, [bindEvent, handleMessageEvent]);
 
   return (
     
